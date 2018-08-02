d5453588caa7e91a2cd29db2f83e9249bb511dcf
김대성 <daeseong@jumong.kr>
2018-08-03 00:46:56 +0900

    TextInput 추가 숫자 전용 입력 추가 MainPage 코드 익명화

---
+++ LayoutPractice/LayoutPractice/Pages/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutPractice
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent ();
+
+            StackLayout ContentStack = new StackLayout();
+            Content = ContentStack;
+            ContentStack.VerticalOptions = LayoutOptions.Center;
+
+            ContentStack.Children.Add(new LayoutButton("StackLayout", new StackPage()));
+            ContentStack.Children.Add(new LayoutButton("Grid", new GridPage()));
+            ContentStack.Children.Add(new LayoutButton("ListViewPage", new ListViewPage()));
+            ContentStack.Children.Add(new LayoutButton("NumberPad", new NumberPad()));
+            ContentStack.Children.Add(new LayoutButton("TextInput", new TextInputPage()));
+        }
+
+        class LayoutButton : Button
+        {
+            public LayoutButton (string Name_p, Page page)
+            {
+                HeightRequest = 50;
+                WidthRequest = 200;
+                Text = Name_p;
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button));
+                BackgroundColor = Color.Aqua;
+
+                this.Clicked += async (sender, e) =>
+                await Navigation.PushAsync(page);
+            }
+        }
+	}
+}
---
+++ LayoutPractice/LayoutPractice/Pages/TextInputPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutPractice
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TextInputPage : ContentPage
+	{
+		public TextInputPage ()
+		{
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent ();
+            backButton.Clicked += async (sender, e) =>
+            await Navigation.PopAsync();
+
+            NumberOnlyEntry.TextChanged += (sender, e) =>
+            {
+                if (e.NewTextValue == null)
+                {
+                    // Completed시 NewTextValue가 null값으로 extChanged하여 진입함
+                    return;
+                }
+                else if (e.NewTextValue == "")
+                {
+                    ((Entry)sender).Text = "";
+                }
+                else if (!(int.TryParse(e.NewTextValue, out int i)))
+                {
+                    ((Entry)sender).Text = e.OldTextValue;
+                }
+            };
+            NumberOnlyEntry.Completed += (sender, e) =>
+            {
+                NumberOnlyLabel.Text = ((Entry)sender).Text;
+                ((Entry)sender).Text = null;
+            };
+
+        }
+	}
+}
